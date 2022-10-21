04e076da1dcd06c60b4f54c1a727d9143060929d
Reception2 <Reception2@DUNLRECEPTILL>
2022-10-21 08:30:44 +0100

    Add project files.

---
+++ LinkEx/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\n************ Query an Array ***********\n");
+            Console.ResetColor();
+            string[] counties = new string[]
+            
+            {
+
+              "Antrim",
+              "Armagh",
+              "Carlow",
+              "Cavan",
+              "Clare",
+              "Cork",
+              "Derry",
+              "Donegal",
+              "Down",
+              "Dublin",
+              "Fermanagh",
+              "Galway",
+              "Kerry",
+              "Kildare",
+              "Kilkenny",
+              "Laois",
+              "Leitrim",
+              "Limerick",
+              "Longford",
+              "Louth",
+              "Mayo",
+              "Meath",
+              "Monaghan",
+              "Offaly",
+              "Roscommon",
+              "Sligo",
+              "Tipperary",
+              "Tyrone",
+              "Waterford",
+              "Westmeath",
+              "Wexford",
+              "Wicklow"
+              
+            };
+
+            var result =
+                from c in counties
+                where c.StartsWith("w".ToUpper())
+                select c;
+
+            foreach(string s in result)
+            {
+                Console.WriteLine(s);
+            }
+
+
+            
+            
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
